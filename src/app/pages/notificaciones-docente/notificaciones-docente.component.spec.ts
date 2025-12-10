4971553119fb823a95d7d86c4267acf8b7648837
street.6 <street.6@hotmail.com>
2025-12-09 22:02:46 -0300

    fix tests

--- src/app/pages/notificaciones-docente/notificaciones-docente.component.spec.ts
+++ src/app/pages/notificaciones-docente/notificaciones-docente.component.spec.ts
@@ -1,14 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotificacionesDocenteComponent } from './notificaciones-docente.component';
+import { NotificacionState } from '../../store/states/api/notificacion.state';
+import { AppPageState } from '../../store/states/page/app.state';
+import { provideStore } from '@ngxs/store';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { NotificacionService } from '../../services/notificacion.service';
+import { of } from 'rxjs';
 
 describe('NotificacionesDocenteComponent', () => {
   let component: NotificacionesDocenteComponent;
   let fixture: ComponentFixture<NotificacionesDocenteComponent>;
 
   beforeEach(async () => {
+    const notificacionServiceMock = {
+      getByDocente: (_: string) => of([]),
+      getNoLeidas: (_: string) => of([]),
+      getNoLeidasCount: (_: string) => of(0),
+      marcarComoLeida: (_: string) => of(void 0),
+      // alumno endpoints not used here but add to satisfy state
+      getByAlumno: (_: string) => of([]),
+      getNoLeidasCountAlumno: (_: string) => of(0),
+    } as Partial<NotificacionService> as NotificacionService;
+
     await TestBed.configureTestingModule({
-      imports: [NotificacionesDocenteComponent]
+      imports: [NotificacionesDocenteComponent],
+      providers: [
+        provideStore([NotificacionState, AppPageState]),
+        provideHttpClient(withInterceptorsFromDi()),
+        MessageService,
+        ConfirmationService,
+        { provide: NotificacionService, useValue: notificacionServiceMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotificacionesDocenteComponent);
--- src/app/pages/novedades-alumno/novedades-alumno.component.spec.ts
+++ src/app/pages/novedades-alumno/novedades-alumno.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NovedadesAlumnoComponent } from './novedades-alumno.component';
+import { NotificacionState } from '../../store/states/api/notificacion.state';
+import { AppPageState } from '../../store/states/page/app.state';
+import { provideStore } from '@ngxs/store';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+import { NotificacionService } from '../../services/notificacion.service';
+import { of } from 'rxjs';
 
 describe('NovedadesAlumnoComponent', () => {
   let component: NovedadesAlumnoComponent;
   let fixture: ComponentFixture<NovedadesAlumnoComponent>;
 
   beforeEach(async () => {
+    const notificacionServiceMock = {
+      getByAlumno: (_: string) => of([]),
+      getNoLeidasCountAlumno: (_: string) => of(0),
+      marcarComoLeida: (_: string) => of(void 0),
+      getByDocente: (_: string) => of([]),
+      getNoLeidas: (_: string) => of([]),
+      getNoLeidasCount: (_: string) => of(0),
+    } as Partial<NotificacionService> as NotificacionService;
     await TestBed.configureTestingModule({
-      imports: [NovedadesAlumnoComponent]
+      imports: [NovedadesAlumnoComponent ],
+      providers: [
+        provideStore([NotificacionState, AppPageState]),
+        provideHttpClient(withInterceptorsFromDi()),
+        MessageService,
+        { provide: NotificacionService, useValue: notificacionServiceMock }
+      ],
     })
     .compileComponents();
     
